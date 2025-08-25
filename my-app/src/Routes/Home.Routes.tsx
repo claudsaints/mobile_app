6abaedfe217d7b41412c187a7cefcb9c8d472890
claudsaints <claudiodsantoss7@gmail.com>
2025-08-25 20:15:52 -0300

    feat: List2 - 03

--- my-app/src/Routes/Home.Routes.tsx
+++ my-app/src/Routes/Home.Routes.tsx
@@ -1,5 +1,6 @@
 //import { createDrawerNavigator, DrawerNavigationProp } from '@react-navigation/Drawer';
 import { createDrawerNavigator, DrawerNavigationProp } from '@react-navigation/drawer';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import Home from '../Screens/Home';
 import One from '../Screens/One';
@@ -13,6 +14,7 @@ import Eight from '../Screens/Eight';
 import Nine from '../Screens/Nine';
 import Ten from '../Screens/Ten';
 import { ParamListBase } from '@react-navigation/native';
+import React from 'react';
 
 export interface RootDrawerParamList extends ParamListBase {
     Um: undefined;
@@ -34,18 +36,89 @@ const Drawer = createDrawerNavigator();
 
 export function HomeDrawerNavigation() {
     return (
-        <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={Home} />
-            <Drawer.Screen name="Um" component={One} />
-            <Drawer.Screen name="Dois" component={Two} />
-            <Drawer.Screen name="Tres" component={Three} />
-            <Drawer.Screen name="Quatro" component={Four} />
-            <Drawer.Screen name="Cinco" component={Five} />
-            <Drawer.Screen name="Seis" component={Six} />
-            <Drawer.Screen name="Sete" component={Seven} />
-            <Drawer.Screen name="Oito" component={Eight} />
-            <Drawer.Screen name="Nove" component={Nine} />
-            <Drawer.Screen name="Dez" component={Ten} />
+        <Drawer.Navigator >
+            <Drawer.Screen
+                options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="home" size={size} color={color} />
+                    ),
+                }}
+                name="Home" component={Home}
+            />
+            <Drawer.Screen
+                options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="local-activity" size={size} color={color} />
+                    ),
+                }}
+                name="Um" component={One}
+            />
+            <Drawer.Screen
+                name="Dois" component={Two}
+                 options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="menu-book" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Drawer.Screen 
+            name="Tres" component={Three}
+             options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="hive" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Drawer.Screen 
+                name="Quatro" component={Four}
+                options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="invert-colors-on" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Drawer.Screen name="Cinco" component={Five}
+                options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="domain" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Drawer.Screen name="Seis" component={Six}
+                options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="inventory" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Drawer.Screen name="Sete" component={Seven}
+                options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="calculate" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Drawer.Screen name="Oito" component={Eight}
+                options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="fingerprint" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Drawer.Screen name="Nove" component={Nine}
+                options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="key" size={size} color={color} />
+                    ),
+                }}
+            />
+            <Drawer.Screen name="Dez" component={Ten}
+                options={{
+                    drawerIcon: ({ focused, color, size }) => (
+                        <Icon name="mail-outline" size={size} color={color} />
+                    ),
+                }}
+            />
         </Drawer.Navigator>
     );
 }
